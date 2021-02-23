52d777c5c886868f269056c7fb949d3798c4ef04
morgan <morganstar@gmail.com>
2021-02-22 23:54:29 -0800

    Generate keypair using user password and Metamask signature

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import owlbob from './owlbob/alfred.svg';
 import './App.css';
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import {Buckets, Client, PrivateKey} from '@textile/hub';
+import { BigNumber, providers, utils } from 'ethers'
+import { hashSync } from 'bcryptjs'
 import {useEffect, useState} from "react";
 import {Button, Collapse} from "react-bootstrap";
 import LetterToTrustedPerson from "./components/LetterToTrustedPerson";
@@ -106,6 +108,48 @@ const insertFile = (buckets, bucketKey, file, path) => {
   })
 }
 
+function generateMessageForEntropy(ethereum_address, application_name, secret) {
+    return (
+        '******************************************************************************** \n' +
+        'READ THIS MESSAGE CAREFULLY. \n' +
+        'DO NOT SHARE THIS SIGNED MESSAGE WITH ANYONE OR THEY WILL HAVE READ AND WRITE \n' +
+        'ACCESS TO THIS APPLICATION. \n' +
+        'DO NOT SIGN THIS MESSAGE IF THE FOLLOWING IS NOT TRUE OR YOU DO NOT CONSENT \n' +
+        'TO THE CURRENT APPLICATION HAVING ACCESS TO THE FOLLOWING APPLICATION. \n' +
+        '******************************************************************************** \n' +
+        'The Ethereum address used by this application is: \n' +
+        '\n' +
+        ethereum_address.value +
+        '\n' +
+        '\n' +
+        '\n' +
+        'By signing this message, you authorize the current application to use the \n' +
+        'following app associated with the above address: \n' +
+        '\n' +
+        application_name +
+        '\n' +
+        '\n' +
+        '\n' +
+        'The hash of your non-recoverable, private, non-persisted password or secret \n' +
+        'phrase is: \n' +
+        '\n' +
+        secret +
+        '\n' +
+        '\n' +
+        '\n' +
+        '******************************************************************************** \n' +
+        'ONLY SIGN THIS MESSAGE IF YOU CONSENT TO THE CURRENT PAGE ACCESSING THE KEYS \n' +
+        'ASSOCIATED WITH THE ABOVE ADDRESS AND APPLICATION. \n' +
+        'AGAIN, DO NOT SHARE THIS SIGNED MESSAGE WITH ANYONE OR THEY WILL HAVE READ AND \n' +
+        'WRITE ACCESS TO THIS APPLICATION. \n' +
+        '******************************************************************************** \n'
+    );
+}
+
+
+
+
+
 function App() {
     return (
         <Router>
@@ -178,6 +222,7 @@ function Alice() {
     const [identity, setIdentity] = useState(null)
     const [buckets, setBuckets] = useState(null);
     const [bucketKey, setBucketKey] = useState(null);
+    const [identityPassword, setIdentityPassword] = useState("");
 
     const [aliceBucketMockup, setAliceBucketMockup] = useState(0);
 
@@ -200,6 +245,69 @@ function Alice() {
     const [docProps, setDocProps] = useState(Object.assign({}, aliceOpolisData))
     deadSetter = () => setDocProps(set(docProps, "member.isDeceased", true))
     props = docProps
+
+    const getSigner = async() => {
+        if (!window.ethereum) {
+            throw new Error(
+                'Ethereum is not connected. Please download Metamask from https://metamask.io/download.html'
+            );
+        }
+
+        console.debug('Initializing web3 provider...');
+        // @ts-ignore
+        const provider = new providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        return signer
+    }
+
+
+    const getAddressAndSigner = async() => {
+        const signer = await getSigner()
+        // @ts-ignore
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (accounts.length === 0) {
+            throw new Error('No account is provided. Please provide an account to this application.');
+        }
+
+        // const address = new EthereumAddress(accounts[0]);
+        const address = accounts[0];
+
+        return {address, signer}
+    }
+
+// async function generatePrivateKey(password) {
+    const generatePrivateKey = async() => {
+        const metamask = await getAddressAndSigner()
+        // avoid sending the raw secret by hashing it first
+        const secret = hashSync(identityPassword, 10)
+        const message = generateMessageForEntropy(metamask.address, 'Endowl POC', secret)
+        const signedText = await metamask.signer.signMessage(message);
+        const hash = utils.keccak256(signedText);
+        if (hash === null) {
+            throw new Error('No account is provided. Please provide an account to this application.');
+        }
+        // The following line converts the hash in hex to an array of 32 integers.
+        // @ts-ignore
+        const array = hash
+            // @ts-ignore
+            .replace('0x', '')
+            // @ts-ignore
+            .match(/.{2}/g)
+            .map((hexNoPrefix) => BigNumber.from('0x' + hexNoPrefix).toNumber())
+
+        if (array.length !== 32) {
+            throw new Error('Hash of signature is not the correct size! Something went wrong!');
+        }
+        const identity = PrivateKey.fromRawEd25519Seed(Uint8Array.from(array))
+        console.log(identity.toString())
+
+        // this.createNotification(identity)
+        console.log(identity.public.toString())
+
+        // Your app can now use this identity for generating a user Mailbox, Threads, Buckets, etc
+        return identity
+    }
+
 
     useEffect(() => {
         async function doAsyncStuff() {
@@ -264,6 +372,14 @@ function Alice() {
                 <h1>Alice's Secure Shared Documents</h1>
 
                 <img src={owlalice} className="App-logo" alt="logo" style={{maxWidth:"200px", height:"auto"}} />
+
+                <p>
+                    Login to Textile
+                </p>
+                <input type="text" placeholder="textile password" value={identityPassword} onChange={e => {setIdentityPassword(e.target.value)}} />
+                <Button variant="primary" onClick={generatePrivateKey}>
+                    Login with Metamask
+                </Button>
 
                 <p>
                     Create document from template:
